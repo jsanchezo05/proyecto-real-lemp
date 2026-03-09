3e3822af5c78561f9715da3c37dd0bdfe943b51e
Juan <juansanchezolivan@gmail.com>
2026-03-09 12:07:38 +0100

    añadir página de contacto

---
+++ html/contacto.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>Página de Contacto</h1>
+    <p>Email: juan@empresa.com</p>
+    <p>Teléfono: 123 456 789</p>
+    <a href="index.php">Volver al inicio</a>
+</body>
+</html>
